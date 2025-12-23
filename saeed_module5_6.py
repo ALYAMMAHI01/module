06b53a0482467d5ba4f02d2c206dfb11fbc049e8
alyammahi01 <bindawei01@gmail.com>
2025-12-23 19:52:15 +0400

    v1

---
+++ saeed_module5_6.py
@@ -0,0 +1,69 @@
+pip install streamlit
+import streamlit as st
+st.title("Welcome to Saeed's Module 5 and 6")
+st.write("This is a simple Streamlit app for demonstration purposes.")
+st.write("You can add more features and functionalities as needed.")
+st.button("Click Me!")
+
+user_question = st.text_input("how are you?")
+if user_question:
+    st.write("fine!, thank you for asking.")
+st.write("Feel free to explore and modify the code!")
+
+else:
+    with st.echo():
+        # This code will be displayed in the app
+        st.title("Welcome to Saeed's Module 5 and 6")
+        st.write("This is a simple Streamlit app for demonstration purposes.")
+        st.write("You can add more features and functionalities as needed.")
+        st.button("Click Me!")
+
+        user_question = st.text_input("how are you?")
+        if user_question:
+            st.write("fine!, thank you for asking.")
+        st.write("Feel free to explore and modify the code!")
+if st.checkbox("Show code"):
+    with st.echo():
+        # This code will be displayed in the app
+        st.title("Welcome to Saeed's Module 5 and 6")
+        st.write("This is a simple Streamlit app for demonstration purposes.")
+        st.write("You can add more features and functionalities as needed.")
+        st.button("Click Me!")
+
+        user_question = st.text_input("how are you?")
+        if user_question:
+            st.write("fine!, thank you for asking.")
+        st.write("Feel free to explore and modify the code!")
+if st.checkbox("Show code"):
+    with st.echo():
+        # This code will be displayed in the app
+        st.title("Welcome to Saeed's Module 5 and 6")
+        st.write("This is a simple Streamlit app for demonstration purposes.")
+        st.write("You can add more features and functionalities as needed.")
+        st.button("Click Me!")
+
+        user_question = st.text_input("how are you?")
+        if user_question:
+            st.write("fine!, thank you for asking.")
+        st.write("Feel free to explore and modify the code!")
+if st.checkbox("Show code"):
+    with st.echo():
+        # This code will be displayed in the app
+        st.title("Welcome to Saeed's Module 5 and 6")
+        st.write("This is a simple Streamlit app for demonstration purposes.")
+        st.write("You can add more features and functionalities as needed.")
+        st.button("Click Me!")
+
+        user_question = st.text_input("how are you?")
+        if user_question:
+            st.write("fine!, thank you for asking.")
+        st.write("Feel free to explore and modify the code!")
+if st.checkbox("Show code"):
+    with st.echo():
+        # This code will be displayed in the app
+        st.title("Welcome to Saeed's Module 5 and 6")
+        st.write("This is a simple Streamlit app for demonstration purposes.")
+        st.write("You can add more features and functionalities as needed.")
+        st.button("Click Me!")
+
+    
